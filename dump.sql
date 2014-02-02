0ce79696547b4607c977bc51a8d46144e51226f9
Mateusz Nowak <mateusz.nowak@xsolve.pl>
2014-02-02 13:18:46 +0100

    Database structure dump update.

--- dump.sql
+++ dump.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.0.10
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Czas wygenerowania: 27 Sty 2014, 20:59
--- Wersja serwera: 5.5.25
--- Wersja PHP: 5.4.4
+-- Czas wygenerowania: 02 Lut 2014, 13:18
+-- Wersja serwera: 5.6.16
+-- Wersja PHP: 5.4.24
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Baza danych: `dev_node`
@@ -20,19 +26,38 @@ SET time_zone = "+00:00";
 -- Struktura tabeli dla tabeli `categories`
 --
 
-CREATE TABLE `categories` (
+CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Zrzut danych tabeli `categories`
+-- Struktura tabeli dla tabeli `orders`
 --
 
-INSERT INTO `categories` (`id`, `name`) VALUES
-(1, 'Akcesoria (Laptop, PC)'),
-(2, 'Części do laptopów');
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `order_items`
+--
+
+CREATE TABLE IF NOT EXISTS `order_items` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int(10) unsigned NOT NULL,
+  `order_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`,`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -40,30 +65,13 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 -- Struktura tabeli dla tabeli `products`
 --
 
-CREATE TABLE `products` (
+CREATE TABLE IF NOT EXISTS `products` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   `price` int(10) unsigned NOT NULL,
   `category_id` int(10) unsigned NOT NULL,
   `description` text NOT NULL,
+  `thumbnail` varchar(256) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `category_id` (`category_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- Zrzut danych tabeli `products`
---
-
-INSERT INTO `products` (`id`, `name`, `price`, `category_id`, `description`) VALUES
-(1, 'Macbook Pro', 1500000, 1, 'MacBook Pro – linia profesjonalnych laptopów w produkcji Apple Inc. Jest ona następczynią modeli z serii PowerBook G4 i jest jednocześnie pierwszym komputerem przenośnym Macintosh, który używa intelowskiego 64-bitowego dwu oraz czterordzeniowego procesora Intel Core i5 oraz i7. Obecnie bazuje na 64 bitowych procesorach intela z platformy Ivy Bridge.');
-
---
--- Ograniczenia dla zrzutów tabel
---
-
---
--- Ograniczenia dla tabeli `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
